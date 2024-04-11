2c4406f38e295d9de3ee63aa391fd21fe0381008
Semenov Vasily <vvs1771@gmail.com>
2024-04-11 22:43:42 +0300

    commit_3

--- main/settings.py
+++ main/settings.py
@@ -120,9 +120,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-try:
-    from .settings_local import *
-except ImportError:
-    pass
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
